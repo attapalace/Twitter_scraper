1a72e8c145d8edeb1114a40f6899f9a9e5d502c1
Ahmed Ata <attapalace@hotmail.com>
2021-06-14 15:25:36 +0200

    Add files via upload

--- Scraping_data_program2.py
+++ Scraping_data_program2.py
@@ -25,6 +25,8 @@ st.markdown('if you want to search for either of multiple terms write as (cats O
 search_term = st.text_input('Enter your search term')
 from_date = st.text_input('Enter starting date as 2020-06-01 foramt', '2020-06-01')
 until_date = st.text_input('Enter end date as 2021-06-01 foramt',today)
+max_results= st.text_input('if you want to show the tweets write the max tweets to display', 100)
+
 
 if st.button('show number of tweets'):  
     if search_term:
@@ -38,3 +40,14 @@ if st.button('show number of tweets'):
             
         st.markdown('Number Of Tweets :'+ str(s))
 
+if st.button('show tweets'):
+    if search_term:
+        all_s = "snscrape --format '{content!r}'"+ f" --max-results {max_results} --since {from_date} twitter-search '{search_term} until:{until_date}' > result-tweets.txt"
+        os.system(all_s)
+        if os.stat("result-tweets.txt").st_size == 0:
+            st.text('No tweets found')
+        else:
+            tweets_df2 = pd.read_csv('result-tweets.txt', names=['content'])
+            for row in tweets_df2['content'].iteritems():
+                st.text(row)
+            st.markdown('Number Of Tweets :'+ str(tweets_df2.size))
